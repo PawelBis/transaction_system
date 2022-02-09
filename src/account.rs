aeb216952113fcd9a0f921834afb5760d8c67657
Paweł Bis <bis1pawel1@gmail.com>
2022-02-09 23:47:11 +0100

    Move account logic to separate file

---
+++ src/account.rs
@@ -0,0 +1,184 @@
+use super::{Transaction, TransactionType};
+use serde::Serialize;
+use std::collections::{HashMap, VecDeque};
+
+#[derive(Default, Debug, Serialize)]
+pub struct Account {
+    client: u16,
+    available: f32,
+    held: f32,
+    total: f32,
+    locked: bool,
+    #[serde(skip_serializing)]
+    pending_transactions: VecDeque<Transaction>,
+    #[serde(skip_serializing)]
+    transactions_history: HashMap<u32, Transaction>,
+}
+
+impl Account {
+    pub fn new(id: u16, initial_transaction: Transaction) -> Self {
+        Self {
+            client: id,
+            pending_transactions: VecDeque::from([initial_transaction]),
+            ..Self::default()
+        }
+    }
+
+    pub fn add_transaction(&mut self, new_transaction: Transaction) {
+        self.pending_transactions.push_back(new_transaction);
+    }
+
+    fn assert_balance(&self) {
+        assert_eq!(self.total, self.available + self.held);
+    }
+
+    fn is_account_state_valid_for_transaction(&self) -> Result<(), String> {
+        if self.locked {
+            Err("Account locked!".into())
+        } else {
+            Ok(())
+        }
+    }
+
+    fn deposit(&mut self, amount: f32) -> Result<(), String> {
+        self.is_account_state_valid_for_transaction()?;
+
+        if amount > 0.0 {
+            self.available += amount;
+            self.total += amount;
+            self.assert_balance();
+            Ok(())
+        } else {
+            Err(format!("deposit amount: {} is not valid", amount).into())
+        }
+    }
+
+    fn withdraw(&mut self, amount: f32) -> Result<(), String> {
+        self.is_account_state_valid_for_transaction()?;
+
+        if amount > 0.0 {
+            if self.available - amount >= 0.0 {
+                self.total -= amount;
+                self.available -= amount;
+                self.assert_balance();
+                Ok(())
+            } else {
+                Err(format!(
+                    "Account available resources: {} are lower than withdraw amount: {}",
+                    self.available, amount
+                )
+                .to_string())
+            }
+        } else {
+            Err(format!("withdraw called with amount {} which is not valid", amount).to_string())
+        }
+    }
+
+    fn dispute(&mut self, transaction_id: u32) -> Result<(), String> {
+        match self.transactions_history.get_mut(&transaction_id) {
+            Some(transaction) => {
+                if transaction.transaction_type == TransactionType::Deposit {
+                    let amount = transaction
+                        .amount
+                        .expect("Transaction stored in transaction_history is valid");
+
+                    transaction.transaction_type = TransactionType::Dispute;
+                    self.available -= amount;
+                    self.held += amount;
+                    self.assert_balance();
+                    Ok(())
+                } else {
+                    Err("Dirpute transaction target was different than Deposit!".into())
+                }
+            }
+            None => Err("Dispute transaction target not valid".into()),
+        }
+    }
+
+    fn find_dispute_transaction(&mut self, dispute_id: u32) -> Result<&mut Transaction, String> {
+        match self.transactions_history.get_mut(&dispute_id) {
+            Some(transaction) => {
+                if transaction.transaction_type != TransactionType::Dispute {
+                    Err("Transaction is not a Dispute transaction".into())
+                } else {
+                    Ok(transaction)
+                }
+            }
+            None => Err(format!(
+                "Transaction with id: {} is not stored in transaction history",
+                dispute_id
+            )),
+        }
+    }
+
+    fn resolve(&mut self, dispute_id: u32) -> Result<(), String> {
+        let dispute_transaction = self.find_dispute_transaction(dispute_id)?;
+        let amount = dispute_transaction
+            .amount
+            .expect("Dispute transaction stored in history contains amount");
+
+        dispute_transaction.transaction_type = TransactionType::Deposit;
+        self.held -= amount;
+        self.available += amount;
+        self.assert_balance();
+        Ok(())
+    }
+
+    fn chargeback(&mut self, dispute_id: u32) -> Result<(), String> {
+        let dispute_transaction = self.find_dispute_transaction(dispute_id)?;
+        let amount = dispute_transaction
+            .amount
+            .expect("Dispute transaction stored in history contains amount");
+
+        dispute_transaction.transaction_type = TransactionType::Chargeback;
+        self.held -= amount;
+        self.total -= amount;
+        self.locked = true;
+        self.assert_balance();
+        Ok(())
+    }
+
+    pub fn process_pending_transaction(&mut self) -> Result<(), String> {
+        self.is_account_state_valid_for_transaction()?;
+        let transaction = match self.pending_transactions.pop_front() {
+            Some(t) => t,
+            None => return Err("Pending queue is empty, cannot process transaction".into()),
+        };
+        match transaction.transaction_type {
+            TransactionType::Deposit => {
+                let amount = match transaction.amount {
+                    Some(a) => a,
+                    None => {
+                        return Err("Deposit is possible only with amount field present".into());
+                    }
+                };
+
+                self.deposit(amount)?;
+                self.transactions_history
+                    .insert(transaction.tx, transaction);
+            }
+            TransactionType::Withdrawal => {
+                let amount = match transaction.amount {
+                    Some(a) => a,
+                    None => {
+                        return Err("Withraw is possible only with amount field present".into());
+                    }
+                };
+
+                self.withdraw(amount)?;
+                self.transactions_history
+                    .insert(transaction.tx, transaction);
+            }
+            TransactionType::Dispute => {
+                self.dispute(transaction.tx)?;
+            }
+            TransactionType::Resolve => {
+                self.resolve(transaction.tx)?;
+            }
+            TransactionType::Chargeback => {
+                self.chargeback(transaction.tx)?;
+            }
+        }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::collections::{HashMap, VecDeque};
 use std::error::Error;
 use std::sync::{Arc, RwLock};
 
+use account::Account;
+mod account;
+
 #[allow(dead_code)]
 #[derive(Debug, Deserialize, PartialEq, Eq)]
 enum TransactionType {
@@ -21,189 +24,12 @@ enum TransactionType {
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug)]
-struct Transaction {
+pub struct Transaction {
     #[serde(rename = "type")]
     transaction_type: TransactionType,
     client: u16,
     tx: u32,
     amount: Option<f32>,
-}
-
-#[derive(Default, Debug, Serialize)]
-struct Account {
-    client: u16,
-    available: f32,
-    held: f32,
-    total: f32,
-    locked: bool,
-    #[serde(skip_serializing)]
-    pending_transactions: VecDeque<Transaction>,
-    #[serde(skip_serializing)]
-    transactions_history: HashMap<u32, Transaction>,
-}
-
-impl Account {
-    fn new(id: u16, initial_transaction: Transaction) -> Self {
-        Self {
-            client: id,
-            pending_transactions: VecDeque::from([initial_transaction]),
-            ..Self::default()
-        }
-    }
-
-    fn assert_balance(&self) {
-        assert_eq!(self.total, self.available + self.held);
-    }
-
-    fn is_account_state_valid_for_transaction(&self) -> Result<(), String> {
-        if self.locked {
-            Err("Account locked!".into())
-        } else {
-            Ok(())
-        }
-    }
-
-    fn deposit(&mut self, amount: f32) -> Result<(), String> {
-        self.is_account_state_valid_for_transaction()?;
-
-        if amount > 0.0 {
-            self.available += amount;
-            self.total += amount;
-            self.assert_balance();
-            Ok(())
-        } else {
-            Err(format!("deposit amount: {} is not valid", amount).into())
-        }
-    }
-
-    fn withdraw(&mut self, amount: f32) -> Result<(), String> {
-        self.is_account_state_valid_for_transaction()?;
-
-        if amount > 0.0 {
-            if self.available - amount >= 0.0 {
-                self.total -= amount;
-                self.available -= amount;
-                self.assert_balance();
-                Ok(())
-            } else {
-                Err(format!(
-                    "Account available resources: {} are lower than withdraw amount: {}",
-                    self.available, amount
-                )
-                .to_string())
-            }
-        } else {
-            Err(format!("withdraw called with amount {} which is not valid", amount).to_string())
-        }
-    }
-
-    fn dispute(&mut self, transaction_id: u32) -> Result<(), String> {
-        match self.transactions_history.get_mut(&transaction_id) {
-            Some(transaction) => {
-                if transaction.transaction_type == TransactionType::Deposit {
-                    let amount = transaction
-                        .amount
-                        .expect("Transaction stored in transaction_history is valid");
-
-                    transaction.transaction_type = TransactionType::Dispute;
-                    self.available -= amount;
-                    self.held += amount;
-                    self.assert_balance();
-                    Ok(())
-                } else {
-                    Err("Dirpute transaction target was different than Deposit!".into())
-                }
-            }
-            None => Err("Dispute transaction target not valid".into()),
-        }
-    }
-
-    fn find_dispute_transaction(&mut self, dispute_id: u32) -> Result<&mut Transaction, String> {
-        match self.transactions_history.get_mut(&dispute_id) {
-            Some(transaction) => {
-                if transaction.transaction_type != TransactionType::Dispute {
-                    Err("Transaction is not a Dispute transaction".into())
-                } else {
-                    Ok(transaction)
-                }
-            }
-            None => Err(format!(
-                "Transaction with id: {} is not stored in transaction history",
-                dispute_id
-            )),
-        }
-    }
-
-    fn resolve(&mut self, dispute_id: u32) -> Result<(), String> {
-        let dispute_transaction = self.find_dispute_transaction(dispute_id)?;
-        let amount = dispute_transaction
-            .amount
-            .expect("Dispute transaction stored in history contains amount");
-
-        dispute_transaction.transaction_type = TransactionType::Deposit;
-        self.held -= amount;
-        self.available += amount;
-        self.assert_balance();
-        Ok(())
-    }
-
-    fn chargeback(&mut self, dispute_id: u32) -> Result<(), String> {
-        let dispute_transaction = self.find_dispute_transaction(dispute_id)?;
-        let amount = dispute_transaction
-            .amount
-            .expect("Dispute transaction stored in history contains amount");
-
-        dispute_transaction.transaction_type = TransactionType::Chargeback;
-        self.held -= amount;
-        self.total -= amount;
-        self.locked = true;
-        self.assert_balance();
-        Ok(())
-    }
-
-    fn process_pending_transaction(&mut self) -> Result<(), String> {
-        self.is_account_state_valid_for_transaction()?;
-        let transaction = match self.pending_transactions.pop_front() {
-            Some(t) => t,
-            None => return Err("Pending queue is empty, cannot process transaction".into()),
-        };
-        match transaction.transaction_type {
-            TransactionType::Deposit => {
-                let amount = match transaction.amount {
-                    Some(a) => a,
-                    None => {
-                        return Err("Deposit is possible only with amount field present".into());
-                    }
-                };
-
-                self.deposit(amount)?;
-                self.transactions_history
-                    .insert(transaction.tx, transaction);
-            }
-            TransactionType::Withdrawal => {
-                let amount = match transaction.amount {
-                    Some(a) => a,
-                    None => {
-                        return Err("Withraw is possible only with amount field present".into());
-                    }
-                };
-
-                self.withdraw(amount)?;
-                self.transactions_history
-                    .insert(transaction.tx, transaction);
-            }
-            TransactionType::Dispute => {
-                self.dispute(transaction.tx)?;
-            }
-            TransactionType::Resolve => {
-                self.resolve(transaction.tx)?;
-            }
-            TransactionType::Chargeback => {
-                self.chargeback(transaction.tx)?;
-            }
-        }
-        Ok(())
-    }
 }
 
 fn deserialize_csv_file(path: String) -> Result<Vec<Transaction>, Box<dyn Error>> {
@@ -233,11 +59,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         match bank.get(&transaction.client) {
             Some(client) => {
-                client
-                    .write()
-                    .unwrap()
-                    .pending_transactions
-                    .push_back(transaction);
+                client.write().unwrap().add_transaction(transaction);
             }
             None => {
                 bank.insert(
